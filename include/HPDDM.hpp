hpddm/hpddm

commit 1f5a71146cf5368152a08a408fddb8e16faa9a07

Enables compilation without C++ RTTI support.
icpc version 16.0.2.181 can now compile the sources.

include/HPDDM.hpp
@@ -201,7 +201,7 @@ template<class T>
 using pod_type = typename std::conditional<std::is_same<underlying_type<T>, T>::value, T, void*>::type;
 
 template<class>
-struct is_substructuring_method : std::false_type { };
+struct is_substructuring_method : public std::false_type { };
 
 template<class T>
 inline void hash_range(std::size_t& seed, T begin, T end) {

include/iterative.hpp
@@ -107,7 +107,7 @@ class IterativeMethod {
         template<class Operator, class K, class T>
         static void addSol(const Operator& A, char variant, const int& n, K* const x, const int& ldh, const K* const s, T* const* const v, const short* const hasConverged, const int& mu, K* const work, const int& deflated = -1) {
             static_assert(std::is_same<K, typename std::remove_const<T>::type>::value, "Wrong types");
-            K* const correction = (variant == 'R' ? (std::is_const<T>::value ? (work + mu * n) : const_cast<K* const>(v[ldh / (deflated == -1 ? mu : deflated) - 1])) : work);
+            K* const correction = (variant == 'R' ? (std::is_const<T>::value ? (work + mu * n) : const_cast<K*>(v[ldh / (deflated == -1 ? mu : deflated) - 1])) : work);
             if(deflated == -1) {
                 int ldv = mu * n;
                 if(variant == 'L') {
@@ -173,7 +173,7 @@ class IterativeMethod {
                 }
                 else {
                     std::copy_n(v[shift], deflated * n, work);
-                    Blas<K>::trmm("R", "U", "N", "N", &n, &deflated, &(Wrapper<K>::d__1), reinterpret_cast<K* const>(norm), &ldh, work, &n);
+                    Blas<K>::trmm("R", "U", "N", "N", &n, &deflated, &(Wrapper<K>::d__1), reinterpret_cast<K*>(norm), &ldh, work, &n);
                     int bK = deflated * shift;
                     Blas<K>::gemm(&(Wrapper<K>::transc), "N", &bK, &deflated, &n, &(Wrapper<K>::d__1), C, &n, work, &n, &(Wrapper<K>::d__0), s, &ldh);
                     for(unsigned short i = 0; i < deflated; ++i)
@@ -238,6 +238,18 @@ class IterativeMethod {
                     else
                         norm[nu] += std::norm(b[nu * n + i]);
                 }
+            }
+        }
+        template<class K>
+        static void epsilon(K& tol, const unsigned char verbosity = 0) {
+            if(std::abs(tol) < std::numeric_limits<underlying_type<K>>::epsilon()) {
+                if(verbosity > 0)
+                    std::cout << "WARNING -- the tolerance of the iterative method was set to " << tol
+#ifdef __cpp_rtti
+                     << " which is lower than the machine epsilon for type " << demangle(typeid(underlying_type<K>).name())
+#endif
+                     << ", forcing the tolerance to " << 2 * std::numeric_limits<underlying_type<K>>::epsilon() << std::endl;
+                tol = 2 * std::numeric_limits<underlying_type<K>>::epsilon();
             }
         }
         /* Function: orthogonalization

include/option.hpp
@@ -320,6 +320,7 @@ class Option : private Singleton {
                             std::cerr << "'" << val << "' doesn't match the regular expression '" << empty << "' for option '" << str << "'" << std::endl;
                     }
                     else if(success) {
+#ifdef __cpp_rtti
                         auto target = std::get<2>(*it).template target<bool (*)(const std::string&, const std::string&, bool)>();
                         if(!target || *target != Arg::argument)
                             map[str] = sto<double>(val);
@@ -331,6 +332,9 @@ class Option : private Singleton {
                                 }
                             map[str + "_" + val] = -static_cast<int>(str.size()) - 10000000;
                         }
+#else
+                        map[str] = sto<double>(val);
+#endif
                     }
                 }
                 else

include/option_impl.hpp
@@ -183,9 +183,13 @@ inline int Option::parse(std::vector<std::string>& args, bool display, const Con
                 std::string val = def.substr(n + 2, def.size() - n - 3);
                 def = def.substr(0, n);
                 if(std::get<2>(x)(def, val, true)) {
+#ifdef __cpp_rtti
                     auto target = std::get<2>(x).template target<bool (*)(const std::string&, const std::string&, bool)>();
                     if(!target || (*target != Arg::argument))
                         (*_app)[def] = sto<double>(val);
+#else
+                    (*_app)[def] = sto<double>(val);
+#endif
                 }
             }
         }
@@ -277,6 +281,19 @@ void Option::version() const {
         " │  penalization: " + std::string(HPDDM_STR(HPDDM_PEN)),
         " │  OpenMP granularity: " + std::string(HPDDM_STR(HPDDM_GRANULARITY)),
         " │  numbering: '" + std::string(1, HPDDM_NUMBERING) + "'",
+        " │  regular expression support? "
+#ifdef HPDDM_NO_REGEX
+            "false",
+#else
+            "true",
+#endif
+        " │  C++ RTTI support? "
+#ifdef __cpp_rtti
+            "true",
+#else
+            "false",
+#pragma message("Consider enabling RTTI support with your C++ compiler")
+#endif
         " │  MKL support? " + std::string(bool(HPDDM_MKL) ? "true" : "false"),
 #ifdef INTEL_MKL_VERSION
         " │  MKL version: " + to_string(INTEL_MKL_VERSION),

.gitattributes
@@ -1,0 +1,1 @@
+Makefile.* linguist-language=Makefile

examples/payload.sh
@@ -36,7 +36,7 @@ function ProgressBar {
 
 TMPFILE=$(mktemp /tmp/hpddm-payload.XXXXXX)
 make clean > /dev/null
-for CXX in g++ clang++ # icpc
+for CXX in g++ clang++ icpc
 do
     I=0
     START=$SECONDS
@@ -53,7 +53,7 @@ do
         export MPICH_CC=icc
         export OMPI_CXX=icpc
         export MPICH_CXX=icpc
-        sed -i\ '' 's/ nullptr>/ (void*)0>/g; s/static constexpr const char/const char/g' include/*.hpp
+        export CXXFLAGS=-D__cpp_rtti
     else
         export OMPI_CC=clang
         export MPICH_CC=clang

include/BDD.hpp
@@ -353,6 +353,6 @@ class Bdd : public Schur<Solver, CoarseOperator<CoarseSolver, S, K>, K> {
 };
 
 template<template<class> class Solver, template<class> class CoarseSolver, char S, class K>
-struct is_substructuring_method<Bdd<Solver, CoarseSolver, S, K>> : std::true_type { };
+struct is_substructuring_method<Bdd<Solver, CoarseSolver, S, K>> : public std::true_type { };
 } // HPDDM
 #endif // _HPDDM_BDD_

include/CG.hpp
@@ -35,13 +35,9 @@ inline int IterativeMethod::CG(const Operator& A, const K* const b, K* const x, 
     const int n = A.getDof();
     const unsigned short it = opt["max_it"];
     underlying_type<K> tol = opt["tol"];
-    const char verbosity = opt.val<char>("verbosity");
+    const unsigned char verbosity = opt.val<unsigned char>("verbosity");
     std::cout << std::scientific;
-    if(std::abs(tol) < std::numeric_limits<underlying_type<K>>::epsilon()) {
-        if(verbosity > 0)
-            std::cout << "WARNING -- the tolerance of the iterative method was set to " << tol << " which is lower than the machine epsilon for type " << demangle(typeid(underlying_type<K>).name()) << ", forcing the tolerance to " << 2 * std::numeric_limits<underlying_type<K>>::epsilon() << std::endl;
-        tol = 2 * std::numeric_limits<underlying_type<K>>::epsilon();
-    }
+    epsilon(tol, verbosity);
     underlying_type<K>* dir;
     K* trash;
     allocate(dir, trash, n, opt["variant"] == 2 ? 2 : 1, it);
@@ -139,13 +135,9 @@ inline int IterativeMethod::PCG(const Operator& A, const K* const f, K* const x,
     const int n = std::is_same<ptr_type, K*>::value ? A.getDof() : A.getMult();
     const unsigned short it = opt["max_it"];
     underlying_type<K> tol = opt["tol"];
-    const char verbosity = opt.val<char>("verbosity");
+    const unsigned char verbosity = opt.val<unsigned char>("verbosity");
     std::cout << std::scientific;
-    if(std::abs(tol) < std::numeric_limits<underlying_type<K>>::epsilon()) {
-        if(verbosity > 0)
-            std::cout << "WARNING -- the tolerance of the iterative method was set to " << tol << " which is lower than the machine epsilon for type " << demangle(typeid(underlying_type<K>).name()) << ", forcing the tolerance to " << 2 * std::numeric_limits<underlying_type<K>>::epsilon() << std::endl;
-        tol = 2 * std::numeric_limits<underlying_type<K>>::epsilon();
-    }
+    epsilon(tol, verbosity);
     const int offset = std::is_same<ptr_type, K*>::value ? A.getEliminated() : 0;
     ptr_type storage[std::is_same<ptr_type, K*>::value ? 1 : 2];
     // storage[0] = r

commit 7a24188c273f136930ce72f97d3fa1cbb8312321

Fixes a regression introduced by commit 1f5a711.
__cpp_rtti may not be defined by some compilers/headers.

include/HPDDM.hpp
@@ -127,6 +127,10 @@ static_assert(HPDDM_NUMBERING == 'C' || HPDDM_NUMBERING == 'F', "Unknown numberi
 # include <algorithm>
 # include <vector>
 # include <numeric>
+# include <functional>
+# if !defined(_cpp_rtti) && !defined(__GXX_RTTI) && !defined(__INTEL_RTTI__) && !defined(_CPPRTTI)
+#  pragma message("Consider enabling RTTI support with your C++ compiler")
+# endif
 static_assert(2 * sizeof(double) == sizeof(std::complex<double>) && 2 * sizeof(float) == sizeof(std::complex<float>) && 2 * sizeof(float) == sizeof(double), "Incorrect sizes");
 # ifdef __GNUG__
 #  include <cxxabi.h>

include/iterative.hpp
@@ -245,7 +245,7 @@ class IterativeMethod {
             if(std::abs(tol) < std::numeric_limits<underlying_type<K>>::epsilon()) {
                 if(verbosity > 0)
                     std::cout << "WARNING -- the tolerance of the iterative method was set to " << tol
-#ifdef __cpp_rtti
+#if defined(_cpp_rtti) || defined(__GXX_RTTI) || defined(__INTEL_RTTI__) || defined(_CPPRTTI)
                      << " which is lower than the machine epsilon for type " << demangle(typeid(underlying_type<K>).name())
 #endif
                      << ", forcing the tolerance to " << 2 * std::numeric_limits<underlying_type<K>>::epsilon() << std::endl;

include/option.hpp
@@ -320,7 +320,7 @@ class Option : private Singleton {
                             std::cerr << "'" << val << "' doesn't match the regular expression '" << empty << "' for option '" << str << "'" << std::endl;
                     }
                     else if(success) {
-#ifdef __cpp_rtti
+#if defined(_cpp_rtti) || defined(__GXX_RTTI) || defined(__INTEL_RTTI__) || defined(_CPPRTTI)
                         auto target = std::get<2>(*it).template target<bool (*)(const std::string&, const std::string&, bool)>();
                         if(!target || *target != Arg::argument)
                             map[str] = sto<double>(val);
@@ -333,7 +333,12 @@ class Option : private Singleton {
                             map[str + "_" + val] = -static_cast<int>(str.size()) - 10000000;
                         }
 #else
-                        map[str] = sto<double>(val);
+                        try {
+                            map[str] = sto<double>(val);
+                        }
+                        catch(const std::invalid_argument& ia) {
+                            std::cerr << "invalid_argument error: " << ia.what() << " (key: " << str << ", value: " << val << ")" << std::endl;
+                        }
 #endif
                     }
                 }

include/option_impl.hpp
@@ -183,7 +183,7 @@ inline int Option::parse(std::vector<std::string>& args, bool display, const Con
                 std::string val = def.substr(n + 2, def.size() - n - 3);
                 def = def.substr(0, n);
                 if(std::get<2>(x)(def, val, true)) {
-#ifdef __cpp_rtti
+#if defined(_cpp_rtti) || defined(__GXX_RTTI) || defined(__INTEL_RTTI__) || defined(_CPPRTTI)
                     auto target = std::get<2>(x).template target<bool (*)(const std::string&, const std::string&, bool)>();
                     if(!target || (*target != Arg::argument))
                         (*_app)[def] = sto<double>(val);
@@ -288,11 +288,10 @@ void Option::version() const {
             "true",
 #endif
         " │  C++ RTTI support? "
-#ifdef __cpp_rtti
+#if defined(_cpp_rtti) || defined(__GXX_RTTI) || defined(__INTEL_RTTI__) || defined(_CPPRTTI)
             "true",
 #else
             "false",
-#pragma message("Consider enabling RTTI support with your C++ compiler")
 #endif
         " │  MKL support? " + std::string(bool(HPDDM_MKL) ? "true" : "false"),
 #ifdef INTEL_MKL_VERSION