hpddm/hpddm

commit 8f6a61959e0c65b724f41d4a9fe5a486c0bef5b9

Solvers are not always necessary.

interface/HPDDM.h
@@ -84,6 +84,7 @@ HpddmMatrixCSR* HpddmMatrixCSRCreate(int, int, int, K*, int*, int*, bool, bool);
 void HpddmMatrixCSRDestroy(HpddmMatrixCSR*);
 void HpddmCsrmm(HpddmMatrixCSR*, const K* const, K*, int);
 
+#if defined(SUBDOMAIN) && defined(COARSEOPERATOR)
 struct HpddmSubdomain;
 typedef struct HpddmSubdomain HpddmSubdomain;
 void HpddmSubdomainNumfact(HpddmSubdomain**, HpddmMatrixCSR*);
@@ -111,6 +112,7 @@ void HpddmSchwarzComputeError(HpddmSchwarz*, const K* const, const K* const, und
 void HpddmSchwarzDestroy(HpddmSchwarz*);
 
 int HpddmSolve(HpddmSchwarz*, const K* const, K* const, int, const MPI_Comm*);
+#endif
 struct HpddmCustomOperator;
 typedef struct HpddmCustomOperator HpddmCustomOperator;
 int HpddmCustomOperatorSolve(const HpddmCustomOperator* const, int, void (*)(const HpddmCustomOperator* const, const K*, K*, int), void (*)(const HpddmCustomOperator* const, const K*, K*, int), const K* const, K* const, int, const MPI_Comm*);

include/iterative.hpp
@@ -33,7 +33,7 @@ struct EmptyOperator {
     void clearBuffer(const bool) const { }
     const underlying_type<K>* getScaling() const { return nullptr; }
 
-    const int _n;
+    int _n;
     EmptyOperator(int n) : _n(n) { }
     int getDof() const { return _n; }
 };

interface/hpddm_c.cpp
@@ -27,10 +27,12 @@ extern "C" {
 #include "HPDDM.h"
 }
 
+#if defined(SUBDOMAIN) && defined(COARSEOPERATOR)
 #ifdef GENERAL_CO
 const char symCoarse = 'G';
 #else
 const char symCoarse = 'S';
+#endif
 #endif
 
 template<class T>
@@ -122,6 +124,7 @@ void HpddmCsrmm(HpddmMatrixCSR* a, const K* const x, K* prod, int m) {
     HPDDM::Wrapper<cpp_type<K>>::csrmm(A->_sym, &(A->_n), &m, A->_a, A->_ia, A->_ja, reinterpret_cast<const cpp_type<K>*>(x), reinterpret_cast<cpp_type<K>*>(prod));
 }
 
+#if defined(SUBDOMAIN) && defined(COARSEOPERATOR)
 void HpddmSubdomainNumfact(HpddmSubdomain** S, HpddmMatrixCSR* Mat) {
     if(Mat) {
         if(*S == NULL)
@@ -186,6 +189,7 @@ void HpddmSchwarzDestroy(HpddmSchwarz* A) {
 int HpddmSolve(HpddmSchwarz* A, const K* const b, K* const sol, int mu, const MPI_Comm* comm) {
     return HPDDM::IterativeMethod::solve(*(reinterpret_cast<HPDDM::Schwarz<SUBDOMAIN, COARSEOPERATOR, symCoarse, cpp_type<K>>*>(A)), reinterpret_cast<const cpp_type<K>*>(b), reinterpret_cast<cpp_type<K>*>(sol), mu, *comm);
 }
+#endif
 int HpddmCustomOperatorSolve(const HpddmCustomOperator* const A, int n, void (*mv)(const HpddmCustomOperator* const, const K*, K*, int), void (*precond)(const HpddmCustomOperator* const, const K*, K*, int), const K* const b, K* const sol, int mu, const MPI_Comm* comm) {
     return HPDDM::IterativeMethod::solve(CustomOperator<HpddmCustomOperator, K>(A, n, mv, precond), reinterpret_cast<const cpp_type<K>*>(b), reinterpret_cast<cpp_type<K>*>(sol), mu, *comm);
 }

interface/hpddm_python.cpp
@@ -44,10 +44,12 @@ typedef double K;
 #endif
 #endif
 
+#if defined(SUBDOMAIN) && defined(COARSEOPERATOR)
 #ifdef GENERAL_CO
 const char symCoarse = 'G';
 #else
 const char symCoarse = 'S';
+#endif
 #endif
 
 struct CustomOperator : HPDDM::CustomOperator<HPDDM::MatrixCSR<K>, K> {
@@ -150,6 +152,7 @@ void csrmm(void* Mat, HPDDM::pod_type<K>* x, HPDDM::pod_type<K>* prod, int m) {
     HPDDM::Wrapper<K>::csrmm(A->_sym, &(A->_n), &m, A->_a, A->_ia, A->_ja, reinterpret_cast<K*>(x), reinterpret_cast<K*>(prod));
 }
 
+#if defined(SUBDOMAIN) && defined(COARSEOPERATOR)
 void subdomainNumfact(void** S, void* Mat) {
     if(Mat) {
         if(*S == NULL)
@@ -236,6 +239,7 @@ void schwarzDestroy(void** schwarz) {
 int solve(void* A, HPDDM::pod_type<K>* f, HPDDM::pod_type<K>* sol, int mu, MPI_Comm* comm) {
     return HPDDM::IterativeMethod::solve(*(reinterpret_cast<HPDDM::Schwarz<SUBDOMAIN, COARSEOPERATOR, symCoarse, K>*>(A)), reinterpret_cast<K*>(f), reinterpret_cast<K*>(sol), mu, *comm);
 }
+#endif
 int CustomOperatorSolve(void* Mat, void (*precond)(const HPDDM::pod_type<K>*, HPDDM::pod_type<K>*, int, int), HPDDM::pod_type<K>* f, HPDDM::pod_type<K>* sol, int n, int mu) {
     return HPDDM::IterativeMethod::solve(CustomOperator(reinterpret_cast<HPDDM::MatrixCSR<K>*>(Mat), precond), reinterpret_cast<K*>(f), reinterpret_cast<K*>(sol), mu, MPI_COMM_SELF);
 }

commit d9d8fa7f9f42ba74fca7258681e52e3dc2cdec80

Fixes a regression introduced by commit 8f6a619.

interface/HPDDM.h
@@ -84,7 +84,6 @@ HpddmMatrixCSR* HpddmMatrixCSRCreate(int, int, int, K*, int*, int*, bool, bool);
 void HpddmMatrixCSRDestroy(HpddmMatrixCSR*);
 void HpddmCsrmm(HpddmMatrixCSR*, const K* const, K*, int);
 
-#if defined(SUBDOMAIN) && defined(COARSEOPERATOR)
 struct HpddmSubdomain;
 typedef struct HpddmSubdomain HpddmSubdomain;
 void HpddmSubdomainNumfact(HpddmSubdomain**, HpddmMatrixCSR*);
@@ -112,7 +111,6 @@ void HpddmSchwarzComputeError(HpddmSchwarz*, const K* const, const K* const, und
 void HpddmSchwarzDestroy(HpddmSchwarz*);
 
 int HpddmSolve(HpddmSchwarz*, const K* const, K* const, int, const MPI_Comm*);
-#endif
 struct HpddmCustomOperator;
 typedef struct HpddmCustomOperator HpddmCustomOperator;
 int HpddmCustomOperatorSolve(const HpddmCustomOperator* const, int, void (*)(const HpddmCustomOperator* const, const K*, K*, int), void (*)(const HpddmCustomOperator* const, const K*, K*, int), const K* const, K* const, int, const MPI_Comm*);