hpddm/hpddm

commit 77addef48b7f96be3535f977c5e2b61d8e1add2d

Adds a ctor for creating matrices from files.
* Some memory allocations in (Block) GCRODR avoided.
* Matrices passed to Dissection must always have diagonal coefficients.
* Removed some MUMPS-specific options set by default.

include/matrix.hpp
@@ -40,6 +40,18 @@ class MatrixCSR {
         bool _free;
 #if INTEL_MKL_VERSION > 110299
 #endif
+        template<class T, typename std::enable_if<!Wrapper<T>::is_complex>::type* = nullptr>
+        static bool scan(const char* str, int* row, int* col, T* val) {
+            int ret = sscanf(str, "%i %i %le", row, col, val);
+            return ret != 3;
+        }
+        template<class T, typename std::enable_if<Wrapper<T>::is_complex>::type* = nullptr>
+        static bool scan(const char* str, int* row, int* col, T* val) {
+            HPDDM::underlying_type<T> re, im;
+            int ret = sscanf(str, "%i %i (%le,%le)", row, col, &re, &im);
+            *val = T(re, im);
+            return ret != 4;
+        }
     public:
         /* Variable: a
          *  Array of data. */
@@ -66,6 +78,72 @@ class MatrixCSR {
         MatrixCSR(const int& n, const int& m, const bool& sym) : _free(true), _a(), _ia(new int[n + 1]), _ja(), _n(n), _m(m), _nnz(0),  _sym(sym) { }
         MatrixCSR(const int& n, const int& m, const int& nnz, const bool& sym) : _free(true), _a(new K[nnz]), _ia(new int[n + 1]), _ja(new int[nnz]), _n(n), _m(m), _nnz(nnz), _sym(sym) { }
         MatrixCSR(const int& n, const int& m, const int& nnz, K* const& a, int* const& ia, int* const& ja, const bool& sym, const bool& takeOwnership = false) : _free(takeOwnership), _a(a), _ia(ia), _ja(ja), _n(n), _m(m), _nnz(nnz), _sym(sym) { }
+        MatrixCSR(std::ifstream& file) {
+            _free = true;
+            if(!file.good()) {
+                _a = nullptr;
+                _ia = nullptr;
+                _ja = nullptr;
+                _n = _m = _nnz = 0;
+            }
+            else {
+                std::string line;
+                _n = 0;
+                _nnz = 0;
+                while(_nnz == 0 && std::getline(file, line)) {
+                    if(line[0] != '#' && line[0] != '%') {
+                        std::stringstream ss(line);
+                        std::istream_iterator<std::string> begin(ss);
+                        std::istream_iterator<std::string> end;
+                        std::vector<std::string> vstrings(begin, end);
+                        if(vstrings.size() == 3) {
+                            _n = sto<int>(vstrings[0]);
+                            _nnz = sto<int>(vstrings[2]);
+                            _sym = false;
+                        }
+                        else if(vstrings.size() > 3) {
+                            _n = sto<int>(vstrings[0]);
+                            _sym = sto<int>(vstrings[2]);
+                            _nnz = sto<int>(vstrings[3]);
+                        }
+                        else {
+                            _a = nullptr;
+                            _ia = nullptr;
+                            _ja = nullptr;
+                            _n = _m = _nnz = 0;
+                        }
+                    }
+                }
+                if(_n && _m) {
+                    std::vector<std::string> parsed;
+                    _ia = new int[_n + 1];
+                    _ja = new int[_nnz];
+                    _a = new K[_nnz];
+                    _ia[0] = (HPDDM_NUMBERING == 'F');
+                    std::fill_n(_ia + 1, _n, 0);
+                    _nnz = 0;
+                    while(std::getline(file, line)) {
+                        int row;
+                        if(scan(line.c_str(), &row, _ja + _nnz, _a + _nnz) && !line.empty() && line[0] != '#' && line[0] != '%') {
+                            delete [] _a;
+                            _a = nullptr;
+                            delete [] _ja;
+                            _ia = nullptr;
+                            delete [] _ia;
+                            _ja = nullptr;
+                            _n = _m = _nnz = 0;
+                            break;
+                        }
+                        if(HPDDM_NUMBERING == 'C')
+                            _ja[_nnz]--;
+                        ++_nnz;
+                        _ia[row]++;
+                    }
+                    if(_ia)
+                        std::partial_sum(_ia, _ia + _n + 1, _ia);
+                }
+            }
+        }
         ~MatrixCSR() {
             destroy();
         }

benchmark/local_solver.cpp
@@ -28,6 +28,8 @@
 #define MKL_INT               int
 #endif
 #define HPDDM_MINIMAL
+#define HPDDM_FETI            0
+#define HPDDM_BDD             0
 #include <HPDDM.hpp>
 
 
@@ -45,19 +47,6 @@ typedef double K;
 #endif
 #endif
 
-template<class K, typename std::enable_if<!HPDDM::Wrapper<K>::is_complex>::type* = nullptr>
-bool scanLine(const char* str, int* row, int* col, K* val) {
-    int ret = sscanf(str, "%i %i %le", row, col, val);
-    return ret != 3;
-}
-template<class K, typename std::enable_if<HPDDM::Wrapper<K>::is_complex>::type* = nullptr>
-bool scanLine(const char* str, int* row, int* col, K* val) {
-    HPDDM::underlying_type<K> re, im;
-    int ret = sscanf(str, "%i %i (%le,%le)", row, col, &re, &im);
-    *val = (re, im);
-    return ret != 4;
-}
-
 int main(int argc, char **argv) {
     if(argc < 2)
         return 1;
@@ -65,51 +54,11 @@ int main(int argc, char **argv) {
     {
         auto tBegin = std::chrono::steady_clock::now();
         std::ifstream t(argv[1]);
-        if(!t.good())
+        A = new HPDDM::MatrixCSR<K>(t);
+        if(A->_n <= 0) {
+            delete A;
             return 1;
-        std::string line;
-        int n = 0;
-        int nnz = 0;
-        bool sym;
-        while(nnz == 0 && std::getline(t, line)) {
-            if(line[0] != '#' && line[0] != '%') {
-                std::stringstream ss(line);
-                std::istream_iterator<std::string> begin(ss);
-                std::istream_iterator<std::string> end;
-                std::vector<std::string> vstrings(begin, end);
-                if(vstrings.size() == 3) {
-                    n = HPDDM::sto<int>(vstrings[0]);
-                    nnz = HPDDM::sto<int>(vstrings[2]);
-                    sym = false;
-                }
-                else if(vstrings.size() > 3) {
-                    n = HPDDM::sto<int>(vstrings[0]);
-                    sym = HPDDM::sto<int>(vstrings[2]);
-                    nnz = HPDDM::sto<int>(vstrings[3]);
-                }
-                else
-                    return 1;
-            }
         }
-        std::vector<std::string> parsed;
-        A = new HPDDM::MatrixCSR<K>(n, n, nnz, sym);
-        A->_ia[0] = (HPDDM_NUMBERING == 'F');
-        std::fill_n(A->_ia + 1, n, 0);
-        nnz = 0;
-        while(std::getline(t, line)) {
-            int row;
-            if(scanLine(line.c_str(), &row, A->_ja + nnz, A->_a + nnz)) {
-                if(!line.empty() && line[0] != '#' && line[0] != '%') {
-                    delete A;
-                    return 1;
-                }
-            }
-            if(HPDDM_NUMBERING == 'C')
-                A->_ja[nnz]--;
-            ++nnz;
-            A->_ia[row]++;
-        }
-        std::partial_sum(A->_ia, A->_ia + n + 1, A->_ia);
         auto tEnd = std::chrono::steady_clock::now();
         std::cout << "// matrix read from file in " << std::chrono::duration<double, std::ratio<1>>(tEnd - tBegin).count() << " second(s)\n";
     }

benchmark/local_solver.py
@@ -38,11 +38,6 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 pair = re.compile(r'\(([^,\)]+),([^,\)]+)\)')
-def parse_pair(s):
-    if match is None:
-        return float(s) + 0j
-    else:
-        return complex(*map(float, match.groups()))
 
 with open(sys.argv[2], 'r') as input:
     k = 0

examples/schwarz.c
@@ -127,7 +127,7 @@ int main(int argc, char **argv) {
         for(unsigned short nu = 0; nu < mu; ++nu)
             nrmb[nu] = nrm2(&ndof, f + nu * ndof, &one);
         K* tmp = malloc(sizeof(K) * mu * ndof);
-        HpddmCsrmm(Mat, sol, tmp, mu);
+        HpddmCSRMM(Mat, sol, tmp, mu);
         K minus = -1;
         ndof *= mu;
         axpy(&ndof, &minus, f, &one, tmp, &one);

examples/solver.py
@@ -30,9 +30,8 @@ import ctypes
 import numpy
 import hpddm
 
-n, m, nnz, a, ia, ja, sym = hpddm.parse_file(sys.argv[1])
-
-Mat = hpddm.matrixCSRCreate(n, m, nnz, a, ia, ja, sym)
+Mat = hpddm.matrixCSRParseFile(ctypes.create_string_buffer(sys.argv[1].encode('ascii', 'ignore')))
+n = hpddm.matrixCSRnRows(Mat)
 S = ctypes.POINTER(hpddm.Subdomain)()
 hpddm.subdomainNumfact(ctypes.byref(S), Mat)
 f = numpy.empty(n, hpddm.scalar)

include/Dissection.hpp
@@ -51,6 +51,41 @@ class DissectionSub {
         void numfact(MatrixCSR<K>* const& A, bool detection = false, K* const& schur = nullptr) {
             static_assert(N == 'C' || N == 'F', "Unknown numbering");
             static_assert(std::is_same<double, underlying_type<K>>::value, "Dissection only supports double-precision floating-point numbers");
+            std::vector<unsigned int> missingCoefficients;
+            for(unsigned int i = 0; i < A->_n; ++i)
+                if(A->_ia[i + 1] == A->_ia[i] || (!A->_sym && !std::binary_search(A->_ja + A->_ia[i] - (N == 'F'), A->_ja + A->_ia[i + 1] - (N == 'F'), i + (N == 'F'))) || (A->_sym && A->_ja[A->_ia[i + 1] - (N == 'F') - 1] - (N == 'F') != i)) {
+                    missingCoefficients.emplace_back(i);
+                }
+            int* ja;
+            K* a;
+            if(!missingCoefficients.empty()) {
+                ja = new int[A->_nnz + missingCoefficients.size()]();
+                a = new K[A->_nnz + missingCoefficients.size()]();
+                unsigned int prev = 0;
+                for(unsigned int i = 0; i < missingCoefficients.size(); ++i) {
+                    std::copy(A->_ja + A->_ia[prev] - (N == 'F'), A->_ja + A->_ia[missingCoefficients[i]] - (N == 'F'), ja + A->_ia[prev] - (N == 'F') + i);
+                    std::copy(A->_a + A->_ia[prev] - (N == 'F'), A->_a + A->_ia[missingCoefficients[i]] - (N == 'F'), a + A->_ia[prev] - (N == 'F') + i);
+                    int dist = !A->_sym ? std::distance(A->_ja + A->_ia[missingCoefficients[i]] - (N == 'F'), std::lower_bound(A->_ja + A->_ia[missingCoefficients[i]] - (N == 'F'), A->_ja + A->_ia[missingCoefficients[i] + 1] - (N == 'F'), missingCoefficients[i] + (N == 'F'))) : A->_ia[missingCoefficients[i] + 1] - A->_ia[missingCoefficients[i]];
+                    std::copy_n(A->_ja + A->_ia[missingCoefficients[i]] - (N == 'F'), dist, ja + A->_ia[missingCoefficients[i]] - (N == 'F') + i);
+                    std::copy_n(A->_a + A->_ia[missingCoefficients[i]] - (N == 'F'), dist, a + A->_ia[missingCoefficients[i]] - (N == 'F') + i);
+                    ja[A->_ia[missingCoefficients[i]] - (N == 'F') + i + dist] = missingCoefficients[i] + (N == 'F');
+                    a[A->_ia[missingCoefficients[i]] - (N == 'F') + i + dist] = K();
+                    if(!A->_sym) {
+                        std::copy_n(A->_ja + A->_ia[missingCoefficients[i]] - (N == 'F') + dist, A->_ia[missingCoefficients[i] + 1] - A->_ia[missingCoefficients[i]] - dist, ja + A->_ia[missingCoefficients[i]] - (N == 'F') + i + dist + 1);
+                        std::copy_n(A->_a + A->_ia[missingCoefficients[i]] - (N == 'F') + dist, A->_ia[missingCoefficients[i] + 1] - A->_ia[missingCoefficients[i]] - dist, a + A->_ia[missingCoefficients[i]] - (N == 'F') + i + dist + 1);
+                    }
+                    std::for_each(A->_ia + prev, A->_ia + missingCoefficients[i] + 1, [&](int& j) { j += i; });
+                    prev = missingCoefficients[i] + 1;
+                }
+                std::copy(A->_ja + A->_ia[prev] - (N == 'F'), A->_ja + A->_nnz, ja + A->_ia[prev] - (N == 'F') + missingCoefficients.size());
+                std::copy(A->_a + A->_ia[prev] - (N == 'F'), A->_a + A->_nnz, a + A->_ia[prev] - (N == 'F') + missingCoefficients.size());
+                std::for_each(A->_ia + prev, A->_ia + A->_n + 1, [&](int& j) { j += missingCoefficients.size(); });
+                A->_nnz += missingCoefficients.size();
+            }
+            else {
+                ja = A->_ja;
+                a = A->_a;
+            }
             if(!_dslv) {
 #ifdef _OPENMP
                 int num_threads = omp_get_max_threads();
@@ -60,15 +95,27 @@ class DissectionSub {
                 _dslv = new DissectionSolver<K, underlying_type<K>>(num_threads, false, 0, nullptr);
                 if(N == 'F') {
                     std::for_each(A->_ia, A->_ia + A->_n + 1, [](int& i) { --i; });
-                    std::for_each(A->_ja, A->_ja + A->_nnz, [](int& i) { --i; });
+                    std::for_each(ja, ja + A->_nnz, [](int& i) { --i; });
                 }
-                _dslv->SymbolicFact(A->_n, A->_ia, A->_ja, A->_sym, false);
+                _dslv->SymbolicFact(A->_n, A->_ia, ja, A->_sym, false);
                 if(N == 'F') {
-                    std::for_each(A->_ja, A->_ja + A->_nnz, [](int& i) { ++i; });
+                    if(missingCoefficients.empty())
+                        std::for_each(ja, ja + A->_nnz, [](int& i) { ++i; });
                     std::for_each(A->_ia, A->_ia + A->_n + 1, [](int& i) { ++i; });
                 }
             }
-            _dslv->NumericFact(0, A->_a, Option::get()->val<char>("dissection_kkt_scaling", 0) ? KKT_SCALING : DIAGONAL_SCALING, Option::get()->val("dissection_pivot_tol", 1.0 / HPDDM_PEN));
+            _dslv->NumericFact(0, a, Option::get()->val<char>("dissection_kkt_scaling", 0) ? KKT_SCALING : DIAGONAL_SCALING, Option::get()->val("dissection_pivot_tol", 1.0 / HPDDM_PEN));
+            if(!missingCoefficients.empty()) {
+                delete [] ja;
+                delete [] a;
+                unsigned int prev = 0;
+                for(unsigned int i = 0; i < missingCoefficients.size(); ++i) {
+                    std::for_each(A->_ia + prev, A->_ia + missingCoefficients[i] + 1, [&](int& j) { j -= i; });
+                    prev = missingCoefficients[i] + 1;
+                }
+                std::for_each(A->_ia + prev, A->_ia + A->_n + 1, [&](int& i) { i -= missingCoefficients.size(); });
+                A->_nnz -= missingCoefficients.size();
+            }
         }
         unsigned short deficiency() const { return _dslv->kern_dimension(); }
         void solve(K* const x, const unsigned short& n = 1) const {

include/GCRODR.hpp
@@ -93,7 +93,7 @@ inline int IterativeMethod::GCRODR(const Operator& A, const K* const b, K* const
     std::cout << std::scientific;
     epsilon(tol, verbosity);
     const unsigned short m = std::min(static_cast<unsigned short>(std::numeric_limits<short>::max()), std::min(opt.val<unsigned short>("gmres_restart", 40), it));
-    k = std::min(m - 1, k);
+    k = std::min(n, std::min(m - 1, k));
     const char variant = opt.val<char>("variant", 1);
 
     const int ldh = mu * (m + 1);
@@ -403,7 +403,7 @@ inline int IterativeMethod::GCRODR(const Operator& A, const K* const b, K* const
                         int dim = std::abs(hasConverged[activeSet[nu]]);
                         for(i = 0; i < dim; ++i)
                             std::fill_n(save[i] + i + 2 + activeSet[nu] * (m + 1), m - i - 1, K());
-                        K* A = new K[dim * dim];
+                        K* A = new K[dim * (dim + 2 + !Wrapper<K>::is_complex)];
                         if(strategy != 1)
                             for(i = 0; i < k; ++i) {
                                 Blas<K>::scal(&n, prod + k * active * (m + 1) + k * nu + i, U + activeSet[nu] * n + i * ldv, &i__1);
@@ -444,7 +444,7 @@ inline int IterativeMethod::GCRODR(const Operator& A, const K* const b, K* const
                             Wrapper<K>::template omatcopy<'C'>(diff, k, H[k] + activeSet[nu] * (m + 1), ldh, B + k, dim);
                             Wrapper<K>::template omatcopy<'C'>(diff, diff, *save + activeSet[nu] * (m + 1), ldh, B + k + k * dim, dim);
                         }
-                        K* alpha = new K[(2 + !Wrapper<K>::is_complex) * dim];
+                        K* alpha = A + dim * dim;
                         int lwork = -1;
                         K* vr = new K[dim * dim];
                         Lapack<K>::ggev("N", "V", &dim, A, &dim, B, &row, alpha, alpha + 2 * dim, alpha + dim, nullptr, &i__1, nullptr, &dim, alpha, &lwork, nullptr, &info);
@@ -455,6 +455,8 @@ inline int IterativeMethod::GCRODR(const Operator& A, const K* const b, K* const
                         std::vector<std::pair<unsigned short, std::complex<underlying_type<K>>>> q;
                         q.reserve(dim);
                         selectNu(opt.val<unsigned short>("recycle_target"), q, dim, alpha, alpha + 2 * dim, alpha + dim);
+                        delete [] B;
+                        delete [] A;
                         info = std::accumulate(q.cbegin(), q.cbegin() + k, 0, [](int a, typename decltype(q)::const_reference b) { return a + b.first; });
                         for(i = k; info != (k * (k - 1)) / 2 && i < dim; ++i)
                             info += q[i].first;
@@ -471,7 +473,6 @@ inline int IterativeMethod::GCRODR(const Operator& A, const K* const b, K* const
                                 Blas<K>::scal(&k, prod + k * active * (m + 1) + k * nu + i, *H + activeSet[nu] * (m + 1) + i, &ldh);
                         Blas<K>::gemm("N", "N", &k, &k, &diff, &(Wrapper<K>::d__1), H[k] + activeSet[nu] * (m + 1), &ldh, vr + k, &dim, &(Wrapper<K>::d__1), *H + activeSet[nu] * (m + 1), &ldh);
                         row = dim + 1;
-                        K* tau = new K[k];
                         *perm = -1;
                         Lapack<K>::geqrf(&row, &k, nullptr, &ldh, nullptr, work, perm, &info);
                         Lapack<K>::mqr("R", "N", &n, &row, &k, nullptr, &ldh, nullptr, nullptr, &ldv, work + 1, perm, &info);
@@ -479,19 +480,15 @@ inline int IterativeMethod::GCRODR(const Operator& A, const K* const b, K* const
                         lwork = std::max(std::real(work[0]), std::real(work[1]));
                         delete [] work;
                         work = new K[lwork];
-                        Lapack<K>::geqrf(&row, &k, *H + activeSet[nu] * (m + 1), &ldh, tau, work, &lwork, &info);
+                        Lapack<K>::geqrf(&row, &k, *H + activeSet[nu] * (m + 1), &ldh, Ax, work, &lwork, &info);
                         Wrapper<K>::template omatcopy<'N'>(k, n, U + activeSet[nu] * n, ldv, v[(m + 1) * (variant == 2)] + activeSet[nu] * n, ldv);
                         Blas<K>::gemm("N", "N", &n, &k, &dim, &(Wrapper<K>::d__1), v[(m + 1) * (variant == 2)] + activeSet[nu] * n, &ldv, vr, &dim, &(Wrapper<K>::d__0), U + activeSet[nu] * n, &ldv);
                         Blas<K>::trsm("R", "U", "N", "N", &n, &k, &(Wrapper<K>::d__1), *H + activeSet[nu] * (m + 1), &ldh, U + activeSet[nu] * n, &ldv);
                         Wrapper<K>::template omatcopy<'N'>(k, n, C + activeSet[nu] * n, ldv, *v + activeSet[nu] * n, ldv);
-                        Lapack<K>::mqr("R", "N", &n, &row, &k, *H + activeSet[nu] * (m + 1), &ldh, tau, *v + activeSet[nu] * n, &ldv, work, &lwork, &info);
+                        Lapack<K>::mqr("R", "N", &n, &row, &k, *H + activeSet[nu] * (m + 1), &ldh, Ax, *v + activeSet[nu] * n, &ldv, work, &lwork, &info);
                         Wrapper<K>::template omatcopy<'N'>(k, n, *v + activeSet[nu] * n, ldv, C + activeSet[nu] * n, ldv);
-                        delete [] tau;
                         delete [] work;
                         delete [] vr;
-                        delete [] alpha;
-                        delete [] B;
-                        delete [] A;
                     }
                 }
                 delete [] prod;
@@ -536,6 +533,7 @@ inline int IterativeMethod::BGCRODR(const Operator& A, const K* const b, K* cons
     std::cout << std::scientific;
     epsilon(tol, verbosity);
     const unsigned short m = std::min(static_cast<unsigned short>(std::numeric_limits<short>::max()), std::min(opt.val<unsigned short>("gmres_restart", 40), it));
+    k = std::min(n, std::min(m - 1, k));
     const char variant = opt.val<char>("variant", 1);
 
     int ldh = mu * (m + 1);
@@ -830,12 +828,10 @@ inline int IterativeMethod::BGCRODR(const Operator& A, const K* const b, K* cons
                     Blas<K>::gemm("N", "N", &n, &bK, &dim, &(Wrapper<K>::d__1), v[(m + 1) * (variant == 2)], &n, vr, &dim, &(Wrapper<K>::d__0), U, &n);
                     Blas<K>::gemm("N", "N", &row, &bK, &dim, &(Wrapper<K>::d__1), *save, &ldh, vr, &dim, &(Wrapper<K>::d__0), *H, &ldh);
                     delete [] vr;
-                    K* tau = new K[bK];
-                    Lapack<K>::geqrf(&row, &bK, *H, &ldh, tau, work, &lwork, &info);
-                    Lapack<K>::mqr("R", "N", &n, &row, &bK, *H, &ldh, tau, *v, &n, work, &lwork, &info);
+                    Lapack<K>::geqrf(&row, &bK, *H, &ldh, Ax, work, &lwork, &info);
+                    Lapack<K>::mqr("R", "N", &n, &row, &bK, *H, &ldh, Ax, *v, &n, work, &lwork, &info);
                     std::copy_n(*v, k * ldv, C);
                     Blas<K>::trsm("R", "U", "N", "N", &n, &bK, &(Wrapper<K>::d__1), *H, &ldh, U, &n);
-                    delete [] tau;
                     delete [] work;
                 }
             }
@@ -857,7 +853,7 @@ inline int IterativeMethod::BGCRODR(const Operator& A, const K* const b, K* cons
                     for(i = 0; i < m - k; ++i)
                         for(unsigned short nu = 0; nu < deflated; ++nu)
                             std::fill(save[i] + nu + 2 + nu * ldh + (i + 1) * deflated, save[i] + (nu + 1) * ldh, K());
-                    K* A = new K[dim * dim];
+                    K* A = new K[dim * (dim + 2 + !Wrapper<K>::is_complex)];
                     if(strategy != 1) {
                         info = dim + deflated;
                         Blas<K>::gemm(&(Wrapper<K>::transc), "N", &info, &bK, &n, &(Wrapper<K>::d__1), *v, &n, U, &n, &(Wrapper<K>::d__0), prod, &info);
@@ -901,15 +897,14 @@ inline int IterativeMethod::BGCRODR(const Operator& A, const K* const b, K* cons
                     else {
                         for(i = 0; i < bK; ++i)
                             B[i * (dim + 1)] = 1.0;
-                        int diff = dim - bK;
                         Wrapper<K>::template omatcopy<'C'>(diff, bK, H[k], ldh, B + bK, dim);
                         Wrapper<K>::template omatcopy<'C'>(diff, diff, *save, ldh, B + bK + bK * dim, dim);
                         row = dim;
                     }
-                    int bDim = dim;
-                    K* alpha = new K[(2 + !Wrapper<K>::is_complex) * bDim];
+                    K* alpha = A + dim * dim;
                     int lwork = -1;
-                    K* vr = new K[bDim * bDim];
+                    K* vr = new K[dim * dim];
+                    int bDim = dim;
                     Lapack<K>::ggev("N", "V", &bDim, A, &bDim, B, &row, alpha, alpha + 2 * bDim, alpha + bDim, nullptr, &i__1, nullptr, &bDim, alpha, &lwork, nullptr, &info);
                     lwork = std::real(*alpha);
                     K* work = new K[Wrapper<K>::is_complex ? (lwork + 4 * bDim) : lwork];
@@ -918,6 +913,8 @@ inline int IterativeMethod::BGCRODR(const Operator& A, const K* const b, K* cons
                     std::vector<std::pair<unsigned short, std::complex<underlying_type<K>>>> q;
                     q.reserve(bDim);
                     selectNu(opt.val<unsigned short>("recycle_target"), q, bDim, alpha, alpha + 2 * bDim, alpha + bDim);
+                    delete [] B;
+                    delete [] A;
                     info = std::accumulate(q.cbegin(), q.cbegin() + bK, 0, [](int a, typename decltype(q)::const_reference b) { return a + b.first; });
                     for(i = bK; info != (bK * (bK - 1)) / 2 && i < bDim; ++i)
                         info += q[i].first;
@@ -934,29 +931,22 @@ inline int IterativeMethod::BGCRODR(const Operator& A, const K* const b, K* cons
                             Blas<K>::scal(&bK, prod + bK * (dim + deflated) + i, *H + i, &ldh);
                     Blas<K>::gemm("N", "N", &bK, &bK, &diff, &(Wrapper<K>::d__1), H[k], &ldh, vr + bK, &bDim, &(Wrapper<K>::d__1), *H, &ldh);
                     row = dim + deflated;
-                    K* tau = new K[bK];
                     *perm = -1;
                     Lapack<K>::geqrf(&row, &bK, nullptr, &ldh, nullptr, work, perm, &info);
                     Lapack<K>::mqr("R", "N", &n, &row, &bK, nullptr, &ldh, nullptr, nullptr, &n, work + 1, perm, &info);
                     delete [] perm;
-                    if(std::real(work[0]) > (Wrapper<K>::is_complex ? (lwork + 4 * bDim) : lwork) || std::real(work[1]) > (Wrapper<K>::is_complex ? (lwork + 4 * bDim) : lwork)) {
-                        lwork = std::max(std::real(work[0]), std::real(work[1]));
-                        delete [] work;
-                        work = new K[lwork];
-                    }
-                    Lapack<K>::geqrf(&row, &bK, *H, &ldh, tau, work, &lwork, &info);
+                    lwork = std::max(std::real(work[0]), std::real(work[1]));
+                    delete [] work;
+                    work = new K[lwork];
+                    Lapack<K>::geqrf(&row, &bK, *H, &ldh, Ax, work, &lwork, &info);
                     Wrapper<K>::template omatcopy<'N'>(bK, n, U, n, v[(m + 1) * (variant == 2)], n);
                     Blas<K>::gemm("N", "N", &n, &bK, &bDim, &(Wrapper<K>::d__1), v[(m + 1) * (variant == 2)], &n, vr, &bDim, &(Wrapper<K>::d__0), U, &n);
                     Blas<K>::trsm("R", "U", "N", "N", &n, &bK, &(Wrapper<K>::d__1), *H, &ldh, U, &n);
                     Wrapper<K>::template omatcopy<'N'>(bK, n, C, n, *v, n);
-                    Lapack<K>::mqr("R", "N", &n, &row, &bK, *H, &ldh, tau, *v, &n, work, &lwork, &info);
+                    Lapack<K>::mqr("R", "N", &n, &row, &bK, *H, &ldh, Ax, *v, &n, work, &lwork, &info);
                     Wrapper<K>::template omatcopy<'N'>(bK, n, *v, n, C, n);
-                    delete [] tau;
                     delete [] work;
                     delete [] vr;
-                    delete [] alpha;
-                    delete [] B;
-                    delete [] A;
                 }
                 delete [] prod;
             }

commit 1fca1ba34a0298787ad1faf2e442d12ebac175c0

Fixes a regression introduced by commit 77addef.

include/matrix.hpp
@@ -84,34 +84,32 @@ class MatrixCSR {
             _free = true;
             if(!file.good()) {
                 _a = nullptr;
-                _ia = nullptr;
-                _ja = nullptr;
+                _ia = _ja = nullptr;
                 _n = _m = _nnz = 0;
             }
             else {
                 std::string line;
-                _n = 0;
-                _nnz = 0;
+                _n = _m = _nnz = 0;
                 while(_nnz == 0 && std::getline(file, line)) {
                     if(line[0] != '#' && line[0] != '%') {
                         std::stringstream ss(line);
-                        std::istream_iterator<std::string> begin(ss);
-                        std::istream_iterator<std::string> end;
+                        std::istream_iterator<std::string> begin(ss), end;
                         std::vector<std::string> vstrings(begin, end);
                         if(vstrings.size() == 3) {
                             _n = sto<int>(vstrings[0]);
+                            _m = sto<int>(vstrings[1]);
                             _nnz = sto<int>(vstrings[2]);
                             _sym = false;
                         }
                         else if(vstrings.size() > 3) {
                             _n = sto<int>(vstrings[0]);
+                            _m = sto<int>(vstrings[1]);
                             _sym = sto<int>(vstrings[2]);
                             _nnz = sto<int>(vstrings[3]);
                         }
                         else {
                             _a = nullptr;
-                            _ia = nullptr;
-                            _ja = nullptr;
+                            _ia = _ja = nullptr;
                             _n = _m = _nnz = 0;
                         }
                     }
@@ -130,9 +128,8 @@ class MatrixCSR {
                             delete [] _a;
                             _a = nullptr;
                             delete [] _ja;
-                            _ia = nullptr;
                             delete [] _ia;
-                            _ja = nullptr;
+                            _ia = _ja = nullptr;
                             _n = _m = _nnz = 0;
                             break;
                         }