32fc0455340501e2c8431c36f2bca533011a4085
Pierre Jolivet <pierre@joliv.et>
2025-05-23 17:45:45 +0200

    Updated PCHPDDM file.

--- .github/workflows/aldaas2021robust.sh
+++ .github/workflows/aldaas2021robust.sh
@@ -23,10 +23,7 @@
 #
 
 cd .. &&  git clone --depth 1 https://github.com/prj-/aldaas2021robust.git && cd - 1> /dev/null
-sed -i -e '/[0-9] KSP/d' ../aldaas2021robust/output/*out
-sed -i -e '/rows=/d' ../aldaas2021robust/output/*out
-sed -i -e '/total: nonzeros=/d' ../aldaas2021robust/output/*out
-sed -i -e '/ I-node /d' ../aldaas2021robust/output/*out
+sed -i -e '/[0-9] KSP/d' -e '/rows=/d' -e '/total: nonzeros=/d' -e '/ I-node /d' ../aldaas2021robust/output/*out
 cat << EOF >> ../aldaas2021robust/sparse_ls.c
 
 /*TEST
@@ -35,7 +32,7 @@ cat << EOF >> ../aldaas2021robust/sparse_ls.c
       requires: hpddm double !complex !defined(PETSC_USE_64BIT_INDICES)
       nsize: 4
       args: -ksp_view -pc_type hpddm
-      filter: egrep -v "[0-9]+ KSP " | grep -v "rows=" | grep -v "total: nonzeros=" | grep -v " I-node " | sed -e "s/CONVERGED_RTOL iterations 6/CONVERGED_RTOL iterations 7/g" -e "s/CONVERGED_RTOL iterations 21/CONVERGED_RTOL iterations 13/g" -e "s/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 8/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 13/g" -e "s/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 12/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 13/g" -e "s/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 28/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 32/g" -e "s/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 43/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 32/g"
+      filter: egrep -v "[0-9]+ KSP " | grep -v "rows=" | grep -v "total: nonzeros=" | grep -v " I-node " | sed -e "s/CONVERGED_RTOL_NORMAL /CONVERGED_RTOL_NORMAL_EQUATIONS /g" -e "s/CONVERGED_RTOL iterations 6/CONVERGED_RTOL iterations 7/g" -e "s/CONVERGED_RTOL iterations 21/CONVERGED_RTOL iterations 13/g" -e "s/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 8/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 13/g" -e "s/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 12/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 13/g" -e "s/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 28/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 32/g" -e "s/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 43/CONVERGED_RTOL_NORMAL_EQUATIONS iterations 32/g"
       test:
         suffix: 1
         args: -options_file \${wPETSC_DIR}/../aldaas2021robust/default.rc -mat_name \${wPETSC_DIR}/../aldaas2021robust/datafiles/mesh_deform.dat -pc_hidden_setup {{true false}shared output}
